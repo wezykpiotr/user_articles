f941c5cf7b3ff0d64a273fe9580f1ea99bcdd61c
Piotr Wezyk <piotr.wezykk@gmail.com>
2022-12-03 19:49:04 +0100

    improving app

--- lib/domain/repositories/articles_repository.dart
+++ lib/domain/repositories/articles_repository.dart
@@ -4,13 +4,18 @@ import 'package:user_articles/domain/models/article_model.dart';
 class ArticlesRepository {
   ArticlesRepository({required this.remoteDataSource});
 
-  final ArticlesMockedDataSource remoteDataSource;
+  final ArticlesRemoteDioDataSource remoteDataSource;
 
   Future<List<ArticleModel>> getArticlesForAuthorId(int authorId) async {
     final json = await remoteDataSource.getArticles();
     if (json == null) {
       return [];
     }
-    return json.map((item) => ArticleModel.fromJson(item)).toList();
+    final allArticles =
+        json.map((item) => ArticleModel.fromJson(item)).toList();
+
+    return allArticles
+        .where((article) => article.authorId == authorId)
+        .toList();
   }
 }
